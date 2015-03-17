6571926551bc2cced3728d3442d9018f12cfd259
ziggy-archi <ziggyarchi@hotmail.fr>
2015-03-17 16:33:04 +0100

    admin

--- oob/admin.py
+++ oob/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from polls.models import Ticket, Personne
+
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 10
+
+
+class PersonneInline(admin.TabularInline):
+    model = Personne
+    extra = 5
--- oob/models.py
+++ oob/models.py
@@ -3,4 +3,10 @@ from django.db import models
 
 
 class Personne(models.Model):
-    nom = models.Charfield(max_lenght=200)
+    nom = models.CharField(max_length=200)
+
+class Ticket(models.Model):
+    demandes = models.CharField(max_length=200)
+    cloture_du_ticket = models.CharField(max_length=20)
+    date_de_soumission = models.DateTimeField()
+    personne = models.ForeignKey(Personne)
